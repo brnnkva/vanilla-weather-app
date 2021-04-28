a91c66427b5f83b978e2fae74cfa930d2a87d7a4
brnnkva <64192884+brnnkva@users.noreply.github.com>
2021-04-28 13:19:32 +0200

    updated current time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
+function formatDate(timestamp){
+    let now = new Date(timestamp);
+
+    let hours = now.getHours();
+    if  (hours < 10) {
+        hours = `0${hours}`;
+      }
+    let minutes = now.getMinutes();
+    if  (minutes < 10) {
+        minutes = `0${minutes}`;
+      }
+      let days = [
+       "sunday",
+       "monday",
+       "tuesday", 
+       "wednesday", 
+       "thursday", 
+       "friday",
+       "saturday"
+       ];
+    let day = days[now.getDay()]; 
+
+    let months = [
+        "january", 
+        "february", 
+        "march", 
+        "april", 
+        "may", 
+        "june", 
+        "july", 
+        "august", 
+        "september", 
+        "october", 
+        "november", 
+        "december"
+        ];
+        let month = months [now.getMonth()]; 
+        let dateNumber = now.getDate();
+     
+       return `${day}, ${month} ${dateNumber} <br> ${hours}:${minutes}`;
+}
+
 function showTemp(response) {
     document.querySelector("#main-city").innerHTML = response.data.name;
     document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
@@ -5,6 +47,9 @@ function showTemp(response) {
   document.querySelector("#weather-discription").innerHTML = response.data.weather[0].description;
   document.querySelector("#wind").innerHTML = response.data.wind.speed;
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+
+  let currentDate = document.querySelector(".current-date");
+  currentDate.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 apiKey = "a2448133104335b630f878b5541b3167";
